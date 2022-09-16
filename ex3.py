942c071121f979cafdfa89f17fd9f0b3cbd7943d
Prashant Singh <mailing2ash@gmail.com>
2022-09-17 00:33:42 +0530

    Initial Commit

---
+++ ex3.py
@@ -0,0 +1,23 @@
+print("I will now count my chickens:") # Printing the string.
+
+print("Hens:", 25 + 30 / 6) # Doing some maths.
+print("Roosters:", 100 / 1 - 25 * 3 % 4) # Doing some maths.
+
+print("Now I will count the eggs:") # Printing the string.
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6) # Doing some maths.
+
+print("Is it true that 3 + 2 < 5 - 7?") # Printing the string.
+
+print(3 + 2 < 5 - 7) # Doing some True or False.
+
+print("What is 3 + 2?", 3 + 2)  # Doing some True or False.
+print("What is 5 - 7?", 5 - 7) # Doing some True or False.
+
+print("Oh, that's why it's False.") # Printing the string.
+
+print("How about some more.") # Printing the string.
+
+print("Is it greater?", 5 > -2) # Doing some True or False.
+print("Is it greater or equal?", 5 >= -2) # Doing some True or False.
+print("Is it less or equal?", 5 <= -2) # Doing some True or False.
