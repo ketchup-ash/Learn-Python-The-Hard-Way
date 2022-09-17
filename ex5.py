6269005cf4d9739453002f0b5cd7d61b9dae4b92
Prashant Singh <mailing2ash@gmail.com>
2022-09-17 21:25:23 +0530

    Variables

---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Prashant Singh'
+my_age = 24
+my_height = 166
+my_weight = 76
+my_eyes = 'Brown'
+my_teeth = 'white'
+my_hair = 'Black'
+
+print(f"Let's talk about {my_name}")
+print(f"He's {my_height} cm tall.")
+print(f"He's {my_weight} kilograms heavy.")
+print(f"Actually that's not heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+# this line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_hair}, and {my_weight} I get {total}.")
